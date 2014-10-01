e9b20b2fa149e93bdc928645b2cc5320d14e08b5
waffles <chriswahlfeldt@gmail.com>
2014-10-01 10:45:51 -0500

    fixed ship model

--- Assets/scripts/CharController.cs
+++ Assets/scripts/CharController.cs
@@ -17,23 +17,28 @@ public class CharController : MonoBehaviour {
 			Plane playerPlane = new Plane(Vector3.up, transform.position);
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			float hitdist = 0.0f;
-//			RaycastHit hit = (RaycastHit) collider.Raycast();
-//			bool check;
-
-//			if (!collider.Raycast(ray, hit, hit.collider.tag == "")) {
 				
 				if (playerPlane.Raycast (ray, out hitdist)) {
 					var targetPoint = ray.GetPoint(hitdist);
 					targetPosition = ray.GetPoint(hitdist);
 					var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
 					transform.rotation = targetRotation;
-				}
-//			}
-
-		}
-
-//		if (hexGen.HitByRay())
+			}
+		}	
 			transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
-
 	}
+
+//	bool ifHit () {
+//			
+//		Plane playerPlane = new Plane(Vector3.up, transform.position);
+//		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//		RaycastHit hit;
+//
+//		if (Physics.Raycast (Ray, hit, playerPlane.Raycast (ray, out hit)))
+//				if (hit.collider.tag.ToLower == "untagged")
+//						return false;
+//				else
+//						return true;
+//
+//	}
 }
