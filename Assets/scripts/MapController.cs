8505f233f63d667382cafa2c681c1c2feb7590a9
waffles <chriswahlfeldt@gmail.com>
2014-10-06 21:17:08 -0500

    Got the board to be somewaht randomly generated

--- Assets/scripts/MapController.cs
+++ Assets/scripts/MapController.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
-using System;
 using System.Collections;
 
 public class MapController : MonoBehaviour {
+	
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +11,23 @@ public class MapController : MonoBehaviour {
 
 		GameObject[] pieces = GameObject.FindGameObjectsWithTag("pieceTag");
 
-		print(pieces[0]);
-		Destroy (pieces [4]);
+		int rando = (int)Random.Range(13f, 19f);
+		int rando1 = (int)Random.Range(12f, 19f);
+		int rando2 = (int)Random.Range(12f, 19f);
+		int rando3 = (int)Random.Range(11f, 19f);
+		int rando4 = (int)Random.Range(12f, 19f);
+		int rando5 = (int)Random.Range(12f, 19f);
+		int rando6 = (int)Random.Range(15f, 19f);
+		int rando7 = (int)Random.Range(12f, 19f);
+
+		Destroy (pieces [rando]);
+		Destroy (pieces [rando1]);
+		Destroy (pieces [rando2]);
+		Destroy (pieces [rando3]);
+		Destroy (pieces [rando4]);
+		Destroy (pieces [rando5]);
+		Destroy (pieces [rando6]);
+		Destroy (pieces [rando7]);
 
 	}
 }
