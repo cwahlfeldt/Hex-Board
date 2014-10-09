56e9d3f0cd810cde96f70bf518616bb3bcab93c0
waffles <chriswahlfeldt@gmail.com>
2014-10-08 19:53:58 -0500

    check it out

--- Assets/scripts/CharController.cs
+++ Assets/scripts/CharController.cs
@@ -8,20 +8,22 @@ public class CharController : MonoBehaviour {
 	
 	public int smooth; // Determines how quickly object moves towards position
 	private Vector3 targetPosition;
-	float speed = 8f;
-	HexGen hexGen;
-	RaycastHit hit;
-	float lastHitDist = 0f;
+	public float speed = 8f;
+	public HexGen hexGen;
+	public RaycastHit hit;
+	public ArrayList hitDistances = new ArrayList();
 	
 
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Mouse0))
 		{
-			Plane playerPlane = new Plane(Vector3.up, transform.position), playerPlane1 = new Plane(Vector3.up, transform.position);
+			Plane playerPlane = new Plane(Vector3.up, transform.position), 
+					playerPlane1 = new Plane(Vector3.up, transform.position);
+
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			Ray ray1 = camera.ScreenPointToRay(Input.mousePosition);
+
 			float hitdist = 0.0f;
-			RaycastHit hit1;
 
 			// checks for distance to tile and sets it to the target position
 			// also sets rotation of object
@@ -29,9 +31,12 @@ public class CharController : MonoBehaviour {
 			&& Physics.Raycast(ray, out hit) 
 			&& !hit.transform.Equals ("")) {
 
+				hitDistances.Add(hitdist);
+
 				GameObject go = GameObject.Find(hit.transform.gameObject.name);
 
-				Vector3 childPieceLocation = new Vector3(go.transform.position.x, go.transform.position.y + 1.3f, go.transform.position.z);
+				Vector3 childPieceLocation = new Vector3
+					(go.transform.position.x, go.transform.position.y + 1.3f, go.transform.position.z);
 
 				var targetPoint = childPieceLocation;
 				targetPosition = childPieceLocation;
@@ -42,12 +47,7 @@ public class CharController : MonoBehaviour {
 			}
 		}	
 
-
-
-		if ((lastHitDist - hit.distance) <= 2 || (lastHitDist - hit.distance) >= -2)
-			transform.position = Vector3.MoveTowards (transform.position, targetPosition, Time.deltaTime * speed);
-		else
-			print("not working");
+		transform.position = Vector3.MoveTowards (transform.position, targetPosition, Time.deltaTime * speed);
 
 	}
 }
