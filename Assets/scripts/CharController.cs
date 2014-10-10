4cdec3c8ebbdc1b469d7adab834a347883a0aeb8
waffles <chriswahlfeldt@gmail.com>
2014-10-10 14:46:45 -0500

    smoothed movement out and cleaned up code

--- Assets/scripts/CharController.cs
+++ Assets/scripts/CharController.cs
@@ -1,80 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Moves the charObject to a tile object when the tile is clicked
 public class CharController: MonoBehaviour {
-	
-	// Click To Move script
-	// Moves the object towards the mouse position on left mouse click
-	
-	public float smooth; // Determines how quickly object moves towards position
-	private Vector3 targetPosition;
+
 	public float speed;
 	public HexGen hexGen;
 	public RaycastHit hit, shiphit;
-	public ArrayList hitDistances;
-	//global index for update var basically finds how man clicks when clicking on object
-	public int i = 0;
-	public Vector3 fwd;
-	GameObject go;
-	Vector3 childPieceLocation;
-	float dist;
-	
+	public GameObject go;
+	public Vector3 childPieceLocation;
+	public float dist;
+	public Vector3 relPosition;
+	public Quaternion quat;
+
 	void Start() {
-		hitDistances = new ArrayList();
-		shiphit.distance = 5f;
-		hitDistances.Add(2f);
+
 		speed = 8f;
-		smooth = 2f;
+
 	}
 	
 	void Update() {
+
+		//finds the ship on update so that it updates the coordinates
 		GameObject ship = GameObject.FindGameObjectWithTag("Player");
-		
+
+		// when mouse button is clicked...(for touch controls CHANGE THIS)
 		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+
+			// creates a plane for the character and it acts as the 'ground'
 			Plane playerPlane = new Plane(Vector3.up, transform.position);
 			
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			//Ray ray1 = camera.ScreenPointToRay(Input.mousePosition);
 			
 			float hitdist = 0.0f;
 			
 			// checks for distance to tile and sets it to the target position
-			// also sets rotation of object
 			if (playerPlane.Raycast(ray, out hitdist) && Physics.Raycast(ray, out hit) && !hit.transform.Equals("")) {
 				
 				go = GameObject.Find(hit.transform.gameObject.name);
 				
-				childPieceLocation = new Vector3(go.transform.position.x, go.transform.position.y + 1.3f, go.transform.position.z);
-				
-				fwd = go.transform.TransformDirection(Vector3.forward);
-				
-				
-				var targetPoint = childPieceLocation;
-				targetPosition = childPieceLocation;
-				
-
-				hitdist = Mathf.Floor(hitdist);
-				
-				hitDistances.Add(hitdist);
-				i++;
+				childPieceLocation = new Vector3(go.transform.position.x, go.transform.position.y + 1.3f, go.transform.position.z);		
 
 				// gets dist from current click
 				dist = Vector3.Distance(ship.transform.position, childPieceLocation);
-				
 
-				
+				// the reletive position of ship to the tile location
+				relPosition = childPieceLocation - ship.transform.position;
+
+				// sets a quaternion where to rotate basd on the relative position 
+				quat = Quaternion.LookRotation (relPosition);
+
 			}
 			
 		}
 
-		Vector3 relPosition = childPieceLocation - ship.transform.position;
-		Quaternion quat = Quaternion.LookRotation (relPosition);
-		Debug.DrawRay(ship.transform.position, childPieceLocation - ship.transform.position, Color.green);
-		
+		// will only move to one tile at a time.
 		if (dist < 3) {
-			transform.rotation = Quaternion.Slerp(ship.transform.rotation, quat, Time.deltaTime * (speed + 9f));
-			transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * (speed - 3f));
 
+			// smooth rotation using slerp
+			transform.rotation = Quaternion.Slerp(ship.transform.rotation, quat, Time.deltaTime * (speed + 9f));
+
+			// smooth transform position using lerp
+			transform.position = Vector3.Lerp(transform.position, childPieceLocation, Time.deltaTime * (speed - 3f));
 		}
 		
 	} // end of update function
--- Assets/scripts/HexGen.cs
+++ Assets/scripts/HexGen.cs
@@ -68,7 +68,7 @@ public class HexGen : MonoBehaviour {
 
 		//add collider to object
 		MeshCollider meshC = go.AddComponent<MeshCollider> ();
-		BoxCollider meshBox = go.AddComponent<BoxCollider> ();
+		go.AddComponent<BoxCollider> ();
 			
 		//create a mesh object to pass our data into
 		Mesh mesh = new Mesh();
@@ -102,8 +102,7 @@ public class HexGen : MonoBehaviour {
 		go.renderer.material.mainTexture = texture;
 	
 	}
-
-
+	
 }
 
 
