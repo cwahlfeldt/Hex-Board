292945b369699de8f378a92f160df9a2bcf79c6f
waffles <chriswahlfeldt@gmail.com>
2014-11-07 14:53:29 -0600

    experimenting

---
+++ Assets/Resources/scripts/LookAtPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find ("Player").GetComponent<CharController> ().isontile == false)
+			transform.LookAt(GameObject.Find ("Player").transform.position, -Vector3.back);
+	}
+}
